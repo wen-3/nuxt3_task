b7b52d0c58a0ddb40ff28b48c2909f1d8e606761
wen-3 <sd63324@gmail.com>
2024-11-28 21:17:07 +0800

    加入 sass
    搭配 bootstrap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: false },
+  css: [
+    '@/assets/stylesheets/all.scss',
+  ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @import "bootstrap/scss/functions";
+            @import "bootstrap/scss/variables";
+          `,
+        },
+      },
+    },
+  },
 })
